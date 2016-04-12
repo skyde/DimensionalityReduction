f6d2af2119e5ea061d0b3aed51008b5d53904021
Sky <skydebreuil@gmail.com>
2016-04-12 15:31:52 +0900

    data

--- Assets/VisualizeData.cs
+++ Assets/VisualizeData.cs
@@ -61,21 +61,64 @@ public class VisualizeData : MonoBehaviour
 
 				v = System.Math.Sqrt(v);
 				point.Distances[i] = v;
+
+//				if(i == 0)
+//				print(v);
 			}
 		}
 	}
 
+	public float Stiffness = 1F;
+	public float Damping = 0.96F;
+	public float DistanceScalar = 1;
+
 	public void Update () 
 	{
-	
+		for (int a = 0; a < Points.Length; a++) 
+		{
+			for (int b = 0; b < Points.Length; b++)
+			{
+				if(a == b)
+				{
+					continue;
+				}
+
+				var desired = (float) Points[a].Distances[b] * DistanceScalar;
+
+				var diff = Points[b].Position - Points[a].Position;
+				var d = diff.magnitude;
+				d -= desired;
+
+				var norm = diff.normalized;//d == 0 ? Vector3.zero : diff / d;
+
+				var impulse = norm * d * Stiffness * 0.0001F;
+
+				Points[a].Velocity += impulse;
+			}
+		}
+//		foreach (var point in Points) 
+//		{
+//			for (int i = 0; i < point.Distances; i++) 
+//			{
+//				
+//			}
+//		}
+
+		foreach (var item in Points) 
+		{
+			item.Velocity *= Damping;
+			item.Position += item.Velocity;
+		}
 	}
+
+	public float PointSize = 0.1F;
 
 	public void OnDrawGizmos()
 	{
 		foreach (var item in Points) 
 		{
 			Gizmos.color = item.Color;
-			Gizmos.DrawSphere(item.Position, 0.02F);
+			Gizmos.DrawSphere(item.Position, PointSize);
 		}
 	}
 }
