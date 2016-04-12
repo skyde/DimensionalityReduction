26273cad4dc8e3507c37fba18e3a49d711496390
Sky <skydebreuil@gmail.com>
2016-04-12 14:38:54 +0900

    load data

--- Assets/ReadMNISTData.cs
+++ Assets/ReadMNISTData.cs
@@ -4,36 +4,62 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine.Assertions;
 
+//[System.Serializable]
+public class Entry
+{
+	public double[] Values;
+}
+
+//[System.Serializable]
+public class Category
+{
+	public Entry[] Entry;
+}
+
 public class ReadMNISTData : MonoBehaviour
 {
 	public TextAsset[] Datas;
+	public Category[] Category;
 
 	public int EntryLength = 28 * 28;
 	
 	public void Awake () 
 	{
-		LoadData(Datas[0]);
+		Category = new Category[Datas.Length];
+
+		var category = new List<Category>();
+
+		for (int i = 0; i < Datas.Length; i++)
+		{
+			category.Add(LoadData(Datas[0]));
+		}
+
+		Category = category.ToArray();
 	}
 
-	public List<double[]> LoadData(TextAsset asset)
+	public Category LoadData(TextAsset asset)
 	{
 		var bytes = asset.bytes;
 
 		Assert.IsTrue(bytes.Length % EntryLength == 0);
 
-		var entries = new List<double[]>();
+		var entries = new List<Entry>();
 		for (int i = 0; i < bytes.Length; i += EntryLength)
 		{
-			var entry = new double[EntryLength];
+			var entry = new Entry();
+			entry.Values = new double[EntryLength];
 
 			for (int e = 0; e < EntryLength; e++) 
 			{
-				entry[e] = (double) bytes[i + e] / 256.0;
+				entry.Values[e] = (double) bytes[i + e] / 256.0;
 			}
 
 			entries.Add(entry);
 		}
 
-		return entries;
+		var category = new Category();
+		category.Entry = entries.ToArray();
+
+		return category;
 	}
 }
