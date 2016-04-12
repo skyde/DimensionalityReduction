3a94d8e83f8c71fec1283ba11dbb08829363d7dd
Sky <skydebreuil@gmail.com>
2016-04-12 15:06:17 +0900

    data

--- Assets/VisualizeData.cs
+++ Assets/VisualizeData.cs
@@ -2,19 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[System.Serializable]
+//[System.Serializable]
 public class VisualizePoint
 {
 	public Color Color;
 	public double[] Distances;
 	public Entry Entry;
+	public Vector2 Position;
+	public Vector2 Velocity;
 }
 
 public class VisualizeData : MonoBehaviour 
 {
 	public int DisplayPerCategory = 100;
 	public ReadMNISTData Data;
-	public VisualizePoint[] Points;
+	public VisualizePoint[] Points = new VisualizePoint[0];
 
 	public void Start()
 	{
@@ -34,10 +36,36 @@ public class VisualizeData : MonoBehaviour
 		}
 
 		Points = points.ToArray();
+
+		foreach (var point in Points) 
+		{
+			point.Distances = new double[Points.Length];
+
+			for (int i = 0; i < Points.Length; i++) 
+			{
+				var p = Points[i];
+
+				if(p == point)
+				{
+					p.Distances[i] = 0;
+				}
+
+				p.Position = Random.insideUnitSphere;
+			}
+		}
 	}
 
 	public void Update () 
 	{
 	
+	}
+
+	public void OnDrawGizmos()
+	{
+		foreach (var item in Points) 
+		{
+			Gizmos.color = item.Color;
+			Gizmos.DrawSphere(item.Position, 0.02F);
+		}
 	}
 }
